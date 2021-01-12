edfbfffbcd095bbcb6a9598d56a2edecf991839a
Tejas Parab <62276215+tejastu@users.noreply.github.com>
2021-01-12 17:33:35 +0530

    Add files via upload

---
+++ 2_print_variables.py
@@ -0,0 +1,21 @@
+a = "Hello Tejas"
+p = 'Hello Tejas'
+q = '''Hello 
+      Tejas'''
+b = 3456
+c = 90.45
+x = True #boolean
+y = None #none
+
+print(a)
+print(p)
+print(q)
+print(b)
+print(c)
+print(x)
+print(y)
+
+
+#printing typeof variable
+print(type(a))
+print(type(y))
